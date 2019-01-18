941b6fb1fa29a6bd50c643ab296fc156d33caf74
aakashDeveloper <ahanda205@gmail.com>
2019-01-18 21:53:51 +0530

    a

---
+++ thirdapp/src/components/ArtistsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ArtistsList = (props) => {
+    return(
+        <h1>Browse the Artists</h1>
+    )
+}
+
+export default ArtistsList;
--- thirdapp/src/components/Home.js
+++ thirdapp/src/components/Home.js
@@ -2,17 +2,33 @@ import React, {Component} from 'react';
 
 //Component
 import Banner from './Banner';
+import ArtistsList from './ArtistsList';
 
 // URL
 const URL_ARTISTS = 'http://localhost:8900/artists';
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            artists:'....loading'
+        }
+    }
     render(){
         return(
             <div>
                 <Banner/>
+                <ArtistsList artsistData ={this.state.artists}/>
             </div>
         )
+    }
+    componentDidMount(){
+        fetch(URL_ARTISTS,{method:'GET'})
+            .then((response) => (response.json()))
+            .then(data => {
+                this.setState({artists:data})
+            })
     }
 }
 
